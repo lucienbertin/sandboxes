2850862d0f465e736a1b1803a0d6816eb588e1ef
Lucien Bertin <me@lucienbertin.com>
2025-02-10 12:10:08 +0100

    add ports models depenent of app models

---
+++ rust/ports/src/models.rs
@@ -0,0 +1,20 @@
+use rocket::serde::{Serialize, Deserialize,};
+
+#[derive(Serialize, Deserialize)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
+
+impl From<application::models::Post> for Post {
+    fn from(value: application::models::Post) -> Self {
+        Self {
+            id: value.id,
+            title: value.title,
+            body: value.body,
+            published: value.published,
+        }
+    }
+}
