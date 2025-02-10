dda54affa71325ace6df9c0e1d126aaeff872494
Lucien Bertin <me@lucienbertin.com>
2025-02-10 12:08:57 +0100

    add adapter dep to app models

--- rust/adapters/src/models.rs
+++ rust/adapters/src/models.rs
@@ -2,6 +2,7 @@ use diesel::prelude::*;
 use postgis_diesel::types::*;
 
 #[derive(Queryable, Selectable)]
+#[derive(Clone)]
 #[diesel(table_name = crate::schema::posts)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Post {
@@ -18,4 +19,26 @@ use crate::schema::posts;
 pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
+}
+
+impl From<application::models::Post> for Post {
+    fn from(value: application::models::Post) -> Self {
+        Self {
+            id: value.id,
+            title: value.title,
+            body: value.body,
+            published: value.published,
+            geom: None, // not binded for now
+        }
+    }
+}
+impl From<Post> for application::models::Post {
+    fn from(value: Post) -> Self {
+        Self {
+            id: value.id,
+            title: value.title,
+            body: value.body,
+            published: value.published,
+        }
+    }
 }
