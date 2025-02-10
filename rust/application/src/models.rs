8be2507256673c4bdae609e2f5e4106f683bb1a1
Lucien Bertin <me@lucienbertin.com>
2025-02-10 12:08:19 +0100

    add app models

---
+++ rust/application/src/models.rs
@@ -0,0 +1,12 @@
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+    // pub geom: Point,
+}
+
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+}
