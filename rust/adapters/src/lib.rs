682177dde5f7d004fe0a3899e349dd176f73c3c4
Lucien Bertin <me@lucienbertin.com>
2025-02-10 15:41:18 +0100

    add post /api/posts route and logic

--- rust/adapters/src/lib.rs
+++ rust/adapters/src/lib.rs
@@ -1,3 +1,4 @@
+use application::models::{NewPost, Post};
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
@@ -30,6 +31,38 @@ pub fn get_published_posts() -> Result<Vec<application::models::Post>, Error> {
     let results: Vec<application::models::Post> = results.into_iter().map(|p| p.into()).collect();
 
     Ok(results)
+}
+
+
+pub fn get_post(post_id: i32) -> Result<Option<application::models::Post>, Error> {
+    use self::schema::posts::dsl::*;
+    use self::models::Post;
+
+    let connection = &mut establish_connection()?;
+    
+    let result: Option<self::models::Post> = posts
+        .find(post_id)
+        .select(Post::as_select())
+        .first(connection)
+        .optional()?;
+    let result = result.map(|p| p.into());
+
+    Ok(result)
+}
+
+pub fn create_post(new_post: NewPost) -> Result<Post, Error> {
+    use self::schema::posts;
+    let connection = &mut establish_connection()?;
+
+    let new_post: self::models::NewPost  = new_post.into();
+
+    let result = diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(self::models::Post::as_returning())
+        .get_result(connection)?;
+
+    Ok(result.into())
+
 }
 
 #[test]
--- rust/ports/src/main.rs
+++ rust/ports/src/main.rs
@@ -1,26 +1,43 @@
 #[macro_use] extern crate rocket;
-use rocket::{http::Status, serde::json::Json};
+use models::NewPost;
+use rocket::{form::Form, http::Status, response::status::Created, serde::json::Json};
 
 pub mod models;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
 
 #[get("/posts", format = "json")]
 pub fn get_posts() -> Result<Json<Vec<self::models::Post>>, rocket::response::status::Custom<&'static str>> {
     use adapters::get_published_posts;
 
-    let results = get_published_posts().map_err(|_e| rocket::response::status::Custom(Status::BadRequest, "error"))?;
+    let results = get_published_posts().map_err(|_e| rocket::response::status::Custom(Status::InternalServerError, "error"))?;
     let results = results.into_iter().map(|x| x.into()).collect();
 
     Ok(Json(results))
 }
 
+#[get("/post/<id>", format = "json")]
+pub fn get_post(id: i32) -> Result<Json<self::models::Post>, rocket::response::status::Custom<&'static str>> {
+    use adapters::get_post;
+
+    let result = get_post(id).map_err(|_e| rocket::response::status::Custom(Status::InternalServerError, "error"))?;
+
+    match result {
+        Some(p) => Ok(Json(p.into())),
+        None => Err(rocket::response::status::Custom(Status::NotFound, "not found"))
+    }
+}
+
+#[post("/posts", data = "<post>")]
+pub fn post_post(post: Form<NewPost>) -> Result<Created<Json<self::models::Post>>, rocket::response::status::Custom<&'static str>> {
+    use adapters::create_post;
+
+    let result = create_post(post.into_inner().into()).map_err(|_e| rocket::response::status::Custom(Status::InternalServerError, "error"))?;
+    let location = format!("/api/post/{}", result.id);
+
+    Ok(Created::new(location))
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, get_posts])
-        // .mount("/api/", routes![get_posts])
+        .mount("/api/", routes![get_posts, get_post, post_post])
 }
