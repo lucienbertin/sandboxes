ed554d44f2b95816f4880b57becabd24e05a6166
Lucien Bertin <me@lucienbertin.com>
2025-02-16 10:33:22 +0100

    add ut to create post use case

--- rust/application/src/usecases/create_post.rs
+++ rust/application/src/usecases/create_post.rs
@@ -1,5 +1,6 @@
 use crate::models::{NewPost, NewPostRequest};
 
+#[derive(PartialEq, Debug)]
 pub enum CreatePostResult {
     DoCreate(NewPost),
 }
@@ -14,5 +15,25 @@ pub fn create_post(subject: String, create_post_request: NewPostRequest) -> Crea
             };
             CreatePostResult::DoCreate(new_post)
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn happy_path() {
+        // arrange
+        let subject = "test@te.st".to_string();
+        let request = NewPostRequest{ title: "test".to_string(), body: "test".to_string() };
+
+        // act
+        let result = create_post(subject.clone(), request);
+
+        // assert
+        assert!(matches!(result, CreatePostResult::DoCreate(_)));
+        let CreatePostResult::DoCreate(new_post) = result;
+        assert_eq!(new_post.author, subject);
     }
 }
