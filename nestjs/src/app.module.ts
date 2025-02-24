57bb7fdcf45e06020741cfe546706b9401f2b468
Lucien Bertin <me@lucienbertin.com>
2025-02-24 15:47:18 +0100

    [nestjs ]add foundations (#7)
    * nest new output
    
    * add typeorm for linking with db
    
    * add basic post entity
    
    * add get posts api exposition
    
    * link to db and finalize setup

---
+++ nestjs/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './post/post.module';
+import { Post } from './post/post.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+      entities: [Post],
+      database: 'postgres',
+      synchronize: false,
+      logging: true,
+    }),
+    PostModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
