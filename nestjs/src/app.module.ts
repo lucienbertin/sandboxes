015500a78e3209575936d79cd79dad95274dac48
Lucien Bertin <me@lucienbertin.com>
2025-02-24 15:42:08 +0100

    link to db and finalize setup

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './post/post.module';
+import { Post } from './post/post.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+      entities: [Post],
+      database: 'postgres',
+      synchronize: false,
+      logging: true,
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
