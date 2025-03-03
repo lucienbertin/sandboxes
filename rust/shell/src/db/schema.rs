14e35da1ee0a4694479e099532bfb2fce5938bfa
Lucien Bertin <me@lucienbertin.com>
2025-03-03 15:24:10 +0100

    add table users in db + diesel schema

--- rust/shell/src/db/schema.rs
+++ rust/shell/src/db/schema.rs
@@ -1,6 +1,10 @@
 // @generated automatically by Diesel CLI.
 
-pub mod sql_types {}
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "user_role"))]
+    pub struct UserRole;
+}
 
 diesel::table! {
     use diesel::sql_types::*;
@@ -15,3 +19,22 @@ diesel::table! {
         author -> Text,
     }
 }
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use postgis_diesel::sql_types::*;
+    use super::sql_types::UserRole;
+
+    users (id) {
+        id -> Int4,
+        first_name -> Text,
+        last_name -> Text,
+        email -> Text,
+        role -> UserRole,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
