2e509175df3524131f2d7bdcf6970bbd87998e78
Lucien Bertin <me@lucienbertin.com>
2025-03-11 15:18:54 +0100

    add statci listing page with all posts

---
+++ nextjs/src/app/posts/page.tsx
@@ -0,0 +1,16 @@
+export default function Posts() {
+    return (
+        <div>
+            <header>
+                ici je mettrais les filtres plus tard
+            </header>
+            <main>
+                <ul>
+                    <li>1er post</li>
+                    <li>2e post</li>
+                    <li>3e post</li>
+                </ul>
+            </main>
+        </div>
+    );
+}
