c88450a2ce9a8e6272bd81856767047e81567a06
Lucien Bertin <me@lucienbertin.com>
2025-03-11 17:17:12 +0100

    add page with all posts listings

--- nextjs/src/app/posts/page.tsx
+++ nextjs/src/app/posts/page.tsx
@@ -1,16 +1,27 @@
-export default function Posts() {
-    return (
-        <div>
-            <header>
-                ici je mettrais les filtres plus tard
-            </header>
-            <main>
-                <ul>
-                    <li>1er post</li>
-                    <li>2e post</li>
-                    <li>3e post</li>
-                </ul>
-            </main>
-        </div>
-    );
+import prisma from "@/lib/prisma";
+
+export default async function Posts() {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+    },
+  });
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)]">
+        Posts
+      </h1>
+      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <span className="font-semibold">{post.title}</span>
+            <span className="text-sm ml-2">
+              by {post.author.firstName} {post.author.lastName} 
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
