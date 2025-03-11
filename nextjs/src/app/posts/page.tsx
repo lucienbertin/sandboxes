b43e5d6703ca0e5875bc0a7d3038bd4a0dbc6499
Lucien Bertin <me@lucienbertin.com>
2025-03-11 18:28:18 +0100

    rem comment

--- nextjs/src/app/posts/page.tsx
+++ nextjs/src/app/posts/page.tsx
@@ -20,18 +20,6 @@ export default function Page() {
       <Suspense fallback={<div>Loading...</div>}>
         <Posts posts$={posts$} />
       </Suspense>
-
-      {/* <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
-        {posts.map((post) => (
-          <li key={post.id}>
-            <span className="font-semibold">{post.title}</span>
-            <span className="text-sm ml-2">
-              by {post.author.firstName} {post.author.lastName} 
-            </span>
-            <Link className="text-sm ml-2" href={`/posts/${post.id}`}> read</Link>
-          </li>
-        ))}
-      </ul> */}
     </div>
   );
 }
