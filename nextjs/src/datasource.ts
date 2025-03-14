0539f0f0fb1eb40cb0b2ce843bba0601dbf7c117
Lucien Bertin <me@lucienbertin.com>
2025-03-14 17:43:46 +0100

    add datasource method to get places as geojson feature collection

--- nextjs/src/datasource.ts
+++ nextjs/src/datasource.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { IPost, Post } from "./post.entity";
 import { IPlace, Place } from "./place.entity";
+import { FeatureCollection, Point } from "geojson";
 
 export const datasource = new DataSource({
     type: 'postgres',
@@ -30,4 +31,12 @@ export async function getPost(id: number): Promise<IPost | null> {
 export async function getPlaces(): Promise<IPlace[]> {
     const places = await datasource.getRepository(Place).find();
     return places.map(p => p.asStruct());
+}
+
+export async function getPlacesGeoJSON(): Promise<FeatureCollection<Point, IPlace>> {
+    const places = await datasource.getRepository(Place).find();
+    return {
+        type: "FeatureCollection",
+        features: places.map(p => p.asGeoJSON()),
+    }
 }
--- nextjs/src/place.entity.ts
+++ nextjs/src/place.entity.ts
@@ -1,19 +1,36 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as typeorm from 'typeorm';
+import { Feature, GeoJSON, Point } from 'geojson';
 
 export interface IPlace {
   id: number;
   name: string;
+  asGeoJSON(): GeoJSON<Point, IPlace>;
 }
 
-@Entity({ name: 'places' })
+@typeorm.Entity({ name: 'places' })
 export class Place implements IPlace {
-  @PrimaryGeneratedColumn()
+  @typeorm.PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ type: 'text' })
+  @typeorm.Column({ type: 'text' })
   name!: string;
+
+  @typeorm.Column({
+    type: 'geography',
+    srid: 4326,
+    spatialFeatureType: 'Point',
+  })
+  geometry!: typeorm.Point;
 
   asStruct(): IPlace {
     return { ...this } as IPlace;
+  }
+  asGeoJSON(): Feature<Point, IPlace> {
+    return {
+      id: this.id,
+      type: 'Feature',
+      properties: this.asStruct(),
+      geometry: this.geometry,
+    }
   }
 }
