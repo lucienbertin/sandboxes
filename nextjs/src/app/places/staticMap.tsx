19a22aea468670352145054398244abd6b857f97
Lucien Bertin <me@lucienbertin.com>
2025-03-15 21:58:37 +0100

    generate static map with mapbox sdk

--- nextjs/src/app/places/staticMap.tsx
+++ nextjs/src/app/places/staticMap.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
+import mapboxClient from "@mapbox/mapbox-sdk/services/static";
 
 export default function StaticMap() {
-    const host = "https://api.mapbox.com/styles/v1";
-    const style = "mapbox/streets-v12";
-    const position = "-1.5536,47.2184,11.5";
-    const width = 960;
-    const height = 640;
-    const token = "pk.eyJ1IjoibHVjaWVuYmVydGluIiwiYSI6ImNsMHJ4cW9idjAyNG4zYnBndXZkeXVuNjEifQ.8gkDcSKIddxBKkwucdo3JA";
-    const src = `${host}/${style}/static/${position}/${width}x${height}?access_token=${token}`;
-    return (
-        <Image
-                width={width}
-                height={height}
-                alt="a static map"
-                src={src} />
-    )
+  const width = 960;
+  const height = 640;
+  const token = "pk.eyJ1IjoibHVjaWVuYmVydGluIiwiYSI6ImNsMHJ4cW9idjAyNG4zYnBndXZkeXVuNjEifQ.8gkDcSKIddxBKkwucdo3JA";
+  
+  const client = mapboxClient({accessToken: token});
+  const request = client.getStaticImage({
+    ownerId: 'mapbox',
+    styleId: 'outdoors-v12',
+    width: width,
+    height: height,
+    position: {
+      coordinates: [-1.5536, 47.2184], // nantes
+      zoom: 11.5
+    }, 
+  });
+  const src = request.url();
+  return (
+    <Image
+    width={width}
+    height={height}
+    alt="a static map"
+    src={src} />
+  )
 }
