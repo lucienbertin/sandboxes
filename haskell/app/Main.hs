6c269b446e4b086bf2969e5ba0d88ed5f6b6a95f
Lucien Bertin <me@lucienbertin.com>
2025-03-28 11:13:23 +0100

    split lib code to a lib andd add foundation for test suite

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,38 +1,8 @@
 module Main where
 
-import Data.Time (LocalTime (LocalTime), dayOfWeek, DayOfWeek( Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday), TimeOfDay (TimeOfDay), getZonedTime, ZonedTime (ZonedTime))
+import Data.Time (TimeOfDay (TimeOfDay))
 
-data DailySchedule = Open | Closed | FromTo TimeOfDay TimeOfDay
-data WeeklySchedule = Week DailySchedule DailySchedule DailySchedule DailySchedule DailySchedule DailySchedule DailySchedule -- 1 dailay schedule per day if the week
-
-data Schedule = DailySchedule DailySchedule | WeeklySchedule WeeklySchedule
-
-isOpenAt :: DailySchedule -> TimeOfDay -> Bool
-isOpenAt Open _= True
-isOpenAt Closed _ = False
-isOpenAt (FromTo from to) t = t >= from && t < to
-
-data WeekTime = WeekTime DayOfWeek TimeOfDay
-weekTime :: LocalTime -> WeekTime
-weekTime (LocalTime date time) = WeekTime (dayOfWeek date) time
-
-isOpenOnDayOfWeek :: WeeklySchedule -> WeekTime -> Bool
-isOpenOnDayOfWeek (Week mon _ _ _ _ _ _) (WeekTime Monday    t) = isOpenAt mon t
-isOpenOnDayOfWeek (Week _ tue _ _ _ _ _) (WeekTime Tuesday   t) = isOpenAt tue t
-isOpenOnDayOfWeek (Week _ _ wed _ _ _ _) (WeekTime Wednesday t) = isOpenAt wed t
-isOpenOnDayOfWeek (Week _ _ _ thu _ _ _) (WeekTime Thursday  t) = isOpenAt thu t
-isOpenOnDayOfWeek (Week _ _ _ _ fri _ _) (WeekTime Friday    t) = isOpenAt fri t
-isOpenOnDayOfWeek (Week _ _ _ _ _ sat _) (WeekTime Saturday  t) = isOpenAt sat t
-isOpenOnDayOfWeek (Week _ _ _ _ _ _ sun) (WeekTime Sunday    t) = isOpenAt sun t
-
-isOpen :: Schedule -> LocalTime -> Bool
-isOpen (DailySchedule s) (LocalTime _ time)  = isOpenAt s time
-isOpen (WeeklySchedule week) dateTime  = isOpenOnDayOfWeek week (weekTime dateTime) 
-
-ioNow :: IO LocalTime
-ioNow = fmap (\(ZonedTime now _tz) -> now) getZonedTime
-isOpenNow :: Schedule -> IO Bool
-isOpenNow s = fmap (isOpen s) ioNow
+import Schedule (ioNow, isOpen, DailySchedule (Open, Closed, FromTo), WeeklySchedule (Week), Schedule (WeeklySchedule, DailySchedule))
 
 main :: IO ()
 main = do
