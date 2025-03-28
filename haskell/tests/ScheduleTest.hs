10111570693c48eba51a9eb7293d442e37a3f365
Lucien Bertin <me@lucienbertin.com>
2025-03-28 16:08:32 +0100

    add UT for it

--- haskell/tests/ScheduleTest.hs
+++ haskell/tests/ScheduleTest.hs
@@ -2,9 +2,10 @@ module Main where
 import Test.HUnit
 import qualified System.Exit as Exit
 
-import Schedule (isOpen, isClosed, DailySchedule (Open, Closed, FromTo), WeeklySchedule (Week), Schedule (WeeklySchedule, DailySchedule))
+import Schedule (isOpen, isClosed, DailySchedule (Open, Closed, FromTo, Switch), WeeklySchedule (Week), Schedule (WeeklySchedule, DailySchedule))
 import Data.Time (Day, TimeOfDay (TimeOfDay), LocalTime (LocalTime), midday, midnight)
 import Data.Time.Calendar.OrdinalDate (fromOrdinalDate)
+import Data.SortedList (toSortedList)
 
 -- date and times
 monday =    fromOrdinalDate 2025 6 -- 1st complete week of 2025
@@ -16,7 +17,9 @@ saturday =  fromOrdinalDate 2025 11
 sunday =    fromOrdinalDate 2025 12
 
 sixAM =   TimeOfDay  6 0 0
-nineAM =  TimeOfDay  9 0 0
+nineAM = TimeOfDay  9 0 0
+onePM = TimeOfDay 13 0 0
+twelveThirtyPM = TimeOfDay 12 30 0
 threePM = TimeOfDay 15 0 0
 fivePM =  TimeOfDay 17 0 0
 eightPM = TimeOfDay 20 0 0
@@ -24,6 +27,7 @@ eightPM = TimeOfDay 20 0 0
 mondayAt6AM = LocalTime monday sixAM
 mondayAt9AM = LocalTime monday nineAM
 mondayAtNoon = LocalTime monday midday
+mondayAtTwelveThirtyPM = LocalTime monday twelveThirtyPM
 mondayAt3PM = LocalTime monday threePM
 mondayAt5PM = LocalTime monday fivePM
 mondayAt8PM = LocalTime monday eightPM
@@ -44,22 +48,37 @@ sundayAtNoon = LocalTime sunday midday
 alwaysOpen = DailySchedule Open
 alwaysClosed = DailySchedule Closed
 nineToFive = FromTo nineAM fivePM
+nineToFiveWithLunchbreak = Switch (toSortedList [nineAM, midday, onePM, fivePM])
 nineToFiveDaily = DailySchedule nineToFive
+nineToFiveWithLunchbreakDaily = DailySchedule nineToFiveWithLunchbreak
 openOnWeekDays = WeeklySchedule (Week Open Open Open Open Open Closed Closed)
 nineToFiveOnWeekDays = WeeklySchedule (Week nineToFive nineToFive nineToFive nineToFive nineToFive Closed Closed)
+
+
 
 -- tests
 -- daily schedules
 dsAlwaysOpenOnMondays = TestCase (assertBool "should be open on any date" (isOpen alwaysOpen mondayAtNoon))
 dsAlwaysClosedOnMondays = TestCase (assertBool "should be close on any date" (isClosed alwaysClosed mondayAtNoon))
 -- FromTo
-dsOpenDuringWorkingHours1 = TestCase (assertBool "should be open @9AM" (isOpen nineToFiveDaily mondayAtNoon))
+dsOpenDuringWorkingHours1 = TestCase (assertBool "should be opening @9AM" (isOpen nineToFiveDaily mondayAt9AM))
 dsOpenDuringWorkingHours2 = TestCase (assertBool "should be open @noon" (isOpen nineToFiveDaily mondayAtNoon))
-dsOpenDuringWorkingHours3 = TestCase (assertBool "should be open @3PM" (isOpen nineToFiveDaily mondayAtNoon))
+dsOpenDuringWorkingHours3 = TestCase (assertBool "should be open @12:30" (isOpen nineToFiveDaily mondayAtTwelveThirtyPM))
+dsOpenDuringWorkingHours4 = TestCase (assertBool "should be open @3PM" (isOpen nineToFiveDaily mondayAt3PM))
 dsClosedOutsideWorkingHours1 = TestCase (assertBool "should be closed @midnight" (isClosed nineToFiveDaily mondayAtNight))
 dsClosedOutsideWorkingHours2 = TestCase (assertBool "should be closed @6AM" (isClosed nineToFiveDaily mondayAt6AM))
 dsClosedOutsideWorkingHours3 = TestCase (assertBool "should be closing @5PM" (isClosed nineToFiveDaily mondayAt5PM))
 dsClosedOutsideWorkingHours4 = TestCase (assertBool "should be closed @8PM" (isClosed nineToFiveDaily mondayAt8PM))
+-- switch
+dsOpenDuringWorkingHoursLunchBreak1 = TestCase (assertBool "should be opening @9AM" (isOpen nineToFiveWithLunchbreakDaily mondayAt9AM))
+dsOpenDuringWorkingHoursLunchBreak2 = TestCase (assertBool "should be closing @noon" (isClosed nineToFiveWithLunchbreakDaily mondayAtNoon))
+dsOpenDuringWorkingHoursLunchBreak3 = TestCase (assertBool "should be closed @3PM" (isClosed nineToFiveWithLunchbreakDaily mondayAtTwelveThirtyPM))
+dsOpenDuringWorkingHoursLunchBreak4 = TestCase (assertBool "should be open @3PM" (isOpen nineToFiveWithLunchbreakDaily mondayAt3PM))
+dsClosedOutsideWorkingHoursLunchBreak1 = TestCase (assertBool "should be closed @midnight" (isClosed nineToFiveWithLunchbreakDaily mondayAtNight))
+dsClosedOutsideWorkingHoursLunchBreak2 = TestCase (assertBool "should be closed @6AM" (isClosed nineToFiveWithLunchbreakDaily mondayAt6AM))
+dsClosedOutsideWorkingHoursLunchBreak3 = TestCase (assertBool "should be closing @5PM" (isClosed nineToFiveWithLunchbreakDaily mondayAt5PM))
+dsClosedOutsideWorkingHoursLunchBreak4 = TestCase (assertBool "should be closed @8PM" (isClosed nineToFiveWithLunchbreakDaily mondayAt8PM))
+
 -- weekly schedules
 wsOpenOnMondays1 = TestCase (assertBool "should be open on a monday" (isOpen openOnWeekDays mondayAtNoon))
 wsOpenOnMondays2 = TestCase (assertBool "should be open on a monday" (isOpen openOnWeekDays mondayAt8PM))
@@ -95,10 +114,19 @@ tests = TestList [
     TestLabel "dsOpenDuringWorkingHours1" dsOpenDuringWorkingHours1,
     TestLabel "dsOpenDuringWorkingHours2" dsOpenDuringWorkingHours2,
     TestLabel "dsOpenDuringWorkingHours3" dsOpenDuringWorkingHours3,
+    TestLabel "dsOpenDuringWorkingHours4" dsOpenDuringWorkingHours4,
     TestLabel "dsClosedOutsideWorkingHours1" dsClosedOutsideWorkingHours1,
     TestLabel "dsClosedOutsideWorkingHours2" dsClosedOutsideWorkingHours2,
     TestLabel "dsClosedOutsideWorkingHours3" dsClosedOutsideWorkingHours3,
     TestLabel "dsClosedOutsideWorkingHours4" dsClosedOutsideWorkingHours4,
+    TestLabel "dsOpenDuringWorkingHoursLunchBreak1" dsOpenDuringWorkingHoursLunchBreak1,
+    TestLabel "dsOpenDuringWorkingHoursLunchBreak2" dsOpenDuringWorkingHoursLunchBreak2,
+    TestLabel "dsOpenDuringWorkingHoursLunchBreak3" dsOpenDuringWorkingHoursLunchBreak3,
+    TestLabel "dsOpenDuringWorkingHoursLunchBreak4" dsOpenDuringWorkingHoursLunchBreak4,
+    TestLabel "dsClosedOutsideWorkingHoursLunchBreak1" dsClosedOutsideWorkingHoursLunchBreak1,
+    TestLabel "dsClosedOutsideWorkingHoursLunchBreak2" dsClosedOutsideWorkingHoursLunchBreak2,
+    TestLabel "dsClosedOutsideWorkingHoursLunchBreak3" dsClosedOutsideWorkingHoursLunchBreak3,
+    TestLabel "dsClosedOutsideWorkingHoursLunchBreak4" dsClosedOutsideWorkingHoursLunchBreak4,
 
     TestLabel "wsOpenOnMondays1" wsOpenOnMondays1,
     TestLabel "wsOpenOnMondays2" wsOpenOnMondays2,
