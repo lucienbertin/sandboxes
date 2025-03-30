df44b9c8011d300012bf4a30f846c80b2ef35fdb
Lucien Bertin <me@lucienbertin.com>
2025-03-30 18:12:46 +0200

    add user domain object

--- nextjs/src/domain/index.ts
+++ nextjs/src/domain/index.ts
@@ -9,3 +9,16 @@ export type Post = {
   body: string;
   published: boolean;
 }
+
+// export enum UserRole {
+//   Reader = 'reader',
+//   Writer = 'writer',
+//   Admin = 'admin',
+// }
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  // role: UserRole;
+}
