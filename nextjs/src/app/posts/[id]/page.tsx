a8724ad3ade3660c43ca2960244684c149222e06
Lucien Bertin <me@lucienbertin.com>
2025-03-31 09:32:16 +0200

    use said action in page instead of a call to db directly

--- nextjs/src/app/posts/[id]/page.tsx
+++ nextjs/src/app/posts/[id]/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { Suspense } from "react";
 import PostComponent from "./post";
-import { getPost } from "@/infrastructure";
+import { getPost } from "@/actions";
+import { redirect } from "next/navigation";
 
 export default async function Page({
   params,
@@ -10,7 +11,7 @@ export default async function Page({
 }) {
   const { id: id_str } = await params;
   const id = parseInt(id_str);
-  const post$ = getPost(id);
+  const post$ = getPost(id).catch(_ => redirect("/"));
 
   return (
     <article className="min-h-screen flex flex-col items-center justify-center -mt-16">
