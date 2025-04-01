de204b97877387660b72cf06b2d46555ad854ae8
Lucien Bertin <me@lucienbertin.com>
2025-04-01 15:44:05 +0200

    fix infra constract

--- nextjs/src/infrastructure/db/post.ts
+++ nextjs/src/infrastructure/db/post.ts
@@ -1,9 +1,8 @@
 "use server";
-import { Post, User } from "@/domain";
-
 import "reflect-metadata";
-import { datasource, isInitialized, ORMPost } from "./datasource";
 import { DeepPartial } from "typeorm";
+import { Post, User } from "@/domain";
+import { datasource, isInitialized, ORMPost, ORMUser } from "./datasource";
 
 export async function getPublishedPosts(): Promise<Post[]> {
   await isInitialized;
@@ -13,6 +12,14 @@ export async function getPublishedPosts(): Promise<Post[]> {
   });
   return posts.map((p) => p.asRecord());
 }
+export async function getPublishedPostsCount(): Promise<number> {
+  await isInitialized;
+  const cnt = await datasource.getRepository(ORMPost).count({
+    where: { published: true },
+  });
+  return cnt;
+}
+
 export async function getMyPostsOrPublishedPosts(me: User): Promise<Post[]> {
   await isInitialized;
   const posts = await datasource.getRepository(ORMPost).find({
@@ -21,6 +28,15 @@ export async function getMyPostsOrPublishedPosts(me: User): Promise<Post[]> {
   });
   return posts.map((p) => p.asRecord());
 }
+export async function getMyPostsOrPublishedPostsCount(me: User): Promise<number> {
+  await isInitialized;
+  const cnt = await datasource.getRepository(ORMPost).count({
+    where: [{ published: true }, { author: { id: me.id } }],
+    relations: { author: true },
+  });
+  return cnt;
+}
+
 export async function getAllPosts(): Promise<Post[]> {
   await isInitialized;
   const posts = await datasource.getRepository(ORMPost).find({
@@ -28,6 +44,15 @@ export async function getAllPosts(): Promise<Post[]> {
   });
   return posts.map((p) => p.asRecord());
 }
+export async function getAllPostsCount(): Promise<number> {
+  await isInitialized;
+  const repo = datasource.getRepository(ORMPost);
+
+  const cnt = await repo.count();
+
+  return cnt;
+}
+
 
 export async function getPost(id: number): Promise<Post | null> {
   await isInitialized;
@@ -39,19 +64,13 @@ export async function getPost(id: number): Promise<Post | null> {
   return post?.asRecord() as Post | null;
 }
 
-export async function createPost(newPost: DeepPartial<Post>) {
+export async function createPost(newPost: DeepPartial<Post>, author: User) {
   await isInitialized;
   const repo = datasource.getRepository(ORMPost);
   const post = repo.create(newPost as DeepPartial<Post>);
 
+  const ormAuthor = await datasource.getRepository(ORMUser).findOneByOrFail({ id: author.id });
+  post.author = ormAuthor;
+
   await repo.save(post);
-}
-
-export async function getPostsCount() {
-  await isInitialized;
-  const repo = datasource.getRepository(ORMPost);
-
-  const cnt = await repo.count();
-
-  return cnt;
 }
