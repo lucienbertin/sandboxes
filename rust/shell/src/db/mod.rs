95a30d959900f37866c6b2ce629ec238193ada89
Lucien Bertin <me@lucienbertin.com>
2025-04-04 14:08:09 +0200

    db add util fn to get conn from pool

--- rust/shell/src/db/mod.rs
+++ rust/shell/src/db/mod.rs
@@ -13,6 +13,7 @@ use std::env;
 
 // Alias to the type for a pool of Diesel PostgreSQL connections.
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbConn = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
 // initializes a data pool
 pub fn init_pool() -> Result<DbPool, Error> {
@@ -22,4 +23,10 @@ pub fn init_pool() -> Result<DbPool, Error> {
     let pool = diesel::r2d2::Pool::new(manager)?;
 
     Ok(pool)
+}
+
+pub fn get_conn(db_pool: &DbPool) -> Result<DbConn, Error> {
+    let conn: DbConn = db_pool.get()?;
+
+    Ok(conn)
 }
