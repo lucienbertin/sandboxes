0e714c5227bf09b8618c25f5958dd1171c55982d
Lucien Bertin <me@lucienbertin.com>
2025-04-08 14:51:26 +0200

    add lazyloading route

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { PostsComponent } from './posts';
 
 export const routes: Routes = [
     { path: 'posts', component: PostsComponent },
+    { path: 'posts-ll', loadComponent: () => import('./posts').then(child => child.PostsComponent) },
     { path: '**', redirectTo: '/posts' },
 ];
