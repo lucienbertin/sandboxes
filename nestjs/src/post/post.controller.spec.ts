417ce73a7bf31e66dd9dbae325e6a1879e9ba43b
Lucien Bertin <me@lucienbertin.com>
2025-04-09 19:13:05 +0200

    [nestjs] fix unit tests and add it to CI (#75)
    * fix UT by injectig mocks
    
    * add UT to CI

--- nestjs/src/post/post.controller.spec.ts
+++ nestjs/src/post/post.controller.spec.ts
@@ -1,6 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { Post } from './post.entity';
+
+class MockPostService {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  public async find(): Promise<Post[]> {
+    return [];
+  }
+}
 
 describe('PostController', () => {
   let controller: PostController;
@@ -8,7 +16,7 @@ describe('PostController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
-      providers: [PostService],
+      providers: [{ provide: PostService, useClass: MockPostService }],
     }).compile();
 
     controller = module.get<PostController>(PostController);
--- nestjs/src/post/post.service.spec.ts
+++ nestjs/src/post/post.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostService } from './post.service';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
+
+class MockPostRepo extends Repository<Post> {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  public async find(): Promise<Post[]> {
+    return [];
+  }
+}
 
 describe('PostService', () => {
   let service: PostService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+      providers: [
+        {
+          provide: 'PostRepository',
+          useClass: MockPostRepo,
+        },
+        PostService,
+      ],
     }).compile();
 
     service = module.get<PostService>(PostService);
