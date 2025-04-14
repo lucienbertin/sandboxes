b8a4028b93bef9fc1eafff31a77fc263fa29f583
Lucien Bertin <me@lucienbertin.com>
2025-04-14 10:55:25 +0200

    add post to graphql exposition

--- nestjs/src/post/post.entity.ts
+++ nestjs/src/post/post.entity.ts
@@ -1,16 +1,22 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity({ name: 'posts' })
 export class Post {
+  @Field(type => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field({ nullable: true })
   @Column({ type: 'text' })
   title: string;
 
+  @Field({ nullable: true })
   @Column({ type: 'text' })
   body: string;
 
+  @Field(type => Boolean)
   @Column({ type: 'boolean' })
   published: boolean;
 }
--- nestjs/src/post/post.module.ts
+++ nestjs/src/post/post.module.ts
@@ -3,10 +3,14 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
+import { PostResolver } from './post.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [
+    PostService,
+    PostResolver,
+  ],
 })
 export class PostModule {}
