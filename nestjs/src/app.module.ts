83767e394d512e0548e0288377d3250ac340ed02
Lucien Bertin <me@lucienbertin.com>
2025-04-14 10:56:20 +0200

    rem rest endpoint

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostModule } from './post';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -26,7 +24,5 @@ import { GraphqlOptions } from './graphql.options';
     }),
     PostModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
