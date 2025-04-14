1ee23e7a6072b402833414dfc068510abec1f337
Lucien Bertin <me@lucienbertin.com>
2025-04-14 16:33:12 +0200

    add cat entity whose source is in mongodb and is exposed by graphql

---
+++ nestjs/src/post/cat.entity.ts
@@ -0,0 +1,32 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+
+export type CatDocument = HydratedDocument<Cat>;
+
+@ObjectType()
+@Schema()
+export class Cat {
+  @Field({ nullable: true })
+  @Prop()
+  name: string;
+
+  @Field(() => Int)
+  @Prop()
+  age: number;
+
+  @Field({ nullable: true })
+  @Prop()
+  breed: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
+
+@InputType()
+export class CreateCatDto {
+  @Field()
+  name: string;
+  @Field()
+  age: number;
+}
