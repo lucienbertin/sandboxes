5a27a6687750476ef7f499347e0903378bc6b53b
Lucien Bertin <me@lucienbertin.com>
2025-04-17 10:16:42 +0200

    basic web expo

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Web.Scotty
 import Schedule (ioNow)
 
+-- main :: IO ()
+-- main = do
+--     now <- ioNow
+--     putStrLn ("Hello, Haskell! it is now: " ++ show now)
+
+
 main :: IO ()
-main = do
-    now <- ioNow
-    putStrLn ("Hello, Haskell! it is now: " ++ show now)
+main = scotty 3000 $
+    get "/:word" $ do
+        beam <- pathParam "word"
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
