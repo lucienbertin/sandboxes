a74f2ddd70772de37b6184ef0c92aac65f425e31
Lucien Bertin <me@lucienbertin.com>
2025-04-18 17:50:17 +0200

    fix imports

--- nextjs/src/actions/place.ts
+++ nextjs/src/actions/place.ts
@@ -1,18 +1,18 @@
 "use server";
 import { Place } from "@/domain";
 import { Feature, FeatureCollection, Point } from "geojson";
-import * as infra from "@/infrastructure";
+import { auth, db } from "@/infrastructure";
 import * as domain from "@/domain";
 
 export async function getPlacesGeoJSON(): Promise<
   FeatureCollection<Point, Place>
 > {
   return domain.getPlacesAsGeoJSON(
-    // infra.resolveAgent,
-    infra.getPlacesGeoJSON,
+    // auth.resolveAgent,
+    db.getPlacesGeoJSON,
   );
 }
 
 export async function createPlace(place: Feature<Point, Partial<Place>>) {
-  return domain.createPlace(place, infra.resolveAgent, infra.createPlace);
+  return domain.createPlace(place, auth.resolveAgent, db.createPlace);
 }
--- nextjs/src/app/api/auth/[...nextauth]/route.ts
+++ nextjs/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/infrastructure";
+import { authOptions } from "@/infrastructure/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
