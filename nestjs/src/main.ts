a0ef51edac4fa752e2e4998f4872ff1224fac0d8
Lucien Bertin <me@lucienbertin.com>
2025-04-18 13:52:52 +0200

    [nestjs] docker (#89)
    * fix vuild shenanigans after #79
    
    * add nestjs dockerfile
    
    * add config via env files
    
    * add nestjs to docker compose
    
    * copy pasta error in nextjs CD
    
    * add CD for nestjs
    
    * rem debug console.log

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -10,6 +11,6 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT as string, process.env.HOST as string);
 }
 bootstrap().catch(() => {});
--- nestjs/src/typeorm.options.ts
+++ nestjs/src/typeorm.options.ts
@@ -1,10 +1,10 @@
 export const typeOrmConfig = {
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
   password: 'nestjs',
   username: 'nestjs',
-  database: 'nestjs',
+  database: 'nestjs-db',
   autoLoadEntities: true,
   synchronize: false,
 };
