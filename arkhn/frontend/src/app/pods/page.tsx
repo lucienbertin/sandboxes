05afaa35ad43d20247c1c968c28fea86efcf829f
Lucien Bertin <me@lucienbertin.com>
2025-04-23 15:41:18 +0200

    pods page

---
+++ arkhn/frontend/src/app/pods/page.tsx
@@ -0,0 +1,21 @@
+import { getPods } from "@/actions"
+
+export default async function Page() {
+    const pods = await getPods();
+    return (
+        <>
+            <article>
+                <header>
+                    <h1>Pods</h1>
+                </header>
+                <section>
+                    <ol>
+                        { pods.map(p => (
+                            <li>ip: {p.ip}, name: {p.name}, phase: {p.phase}</li>
+                        ))}
+                    </ol>
+                </section>
+            </article>
+        </>
+    )
+}
