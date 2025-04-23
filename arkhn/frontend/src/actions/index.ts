e2d8496131418b80f93ae857d7446d95db6f5abb
Lucien Bertin <me@lucienbertin.com>
2025-04-23 15:41:00 +0200

    server actions to consumme backend api

---
+++ arkhn/frontend/src/actions/index.ts
@@ -0,0 +1,40 @@
+"use server"
+export async function getPods() {
+    const response = await fetch("http://127.0.0.1:8000/pods");
+    const data = await response.json();
+    return data;
+}
+
+export async function createDeployment(formData) {
+    const name = formData.get('name');
+    const image = formData.get('image');
+    const replicas = parseInt(formData.get('replicas'), 10);
+    const body = JSON.stringify({
+        name: name,
+        image: image,
+        replicas: replicas
+    });
+    await fetch(`http://127.0.0.1:8000/deployments`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: body
+    });
+}
+export async function getDeployments() {
+    const response = await fetch("http://127.0.0.1:8000/deployments");
+    const data = await response.json();
+    return data;
+}
+export async function getDeployment(name: string) {
+    const response = await fetch(`http://127.0.0.1:8000/deployment/${name}`);
+    const data = await response.json();
+    return data;
+}
+
+export async function deleteDeployment(name: string) {
+    const response = await fetch(`http://127.0.0.1:8000/deployment/${name}`, {
+        method: "DELETE"
+    });
+}
