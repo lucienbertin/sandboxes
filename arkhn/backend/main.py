f60511a3a1e55a39fae00c68e990561df8cfd196
Lucien Bertin <me@lucienbertin.com>
2025-04-23 10:23:43 +0200

    add py-hubernetes and expose list of pods in namespace default

--- arkhn/backend/main.py
+++ arkhn/backend/main.py
@@ -1,9 +1,10 @@
 from typing import Union
-
+from kubernetes import client, config
 from fastapi import FastAPI
-
+config.load_kube_config()
 app = FastAPI()
 
+v1 = client.CoreV1Api()
 
 @app.get("/")
 def read_root():
@@ -13,3 +14,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/pods")
+def get_pods():
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    public_pods = filter(lambda p : p.metadata.namespace == "default", ret.items)
+    for pod in public_pods:
+        return { "ip": pod.status.pod_ip, "name": pod.metadata.name, "phase": pod.status.phase }
