54502df82287730c5e0e414e890af8b411715ec2
Lucien Bertin <me@lucienbertin.com>
2025-04-24 19:14:15 +0200

    [blazor] scaffold (#99)
    * what the cli spat out
    with some minor changes (like gitignoring bootstrap dist)
    
    * rem import to bootstrap css
    
    * rem bootstrap css from gitignore
    
    * try add CI for blazor project
    
    * add dockerfile
    
    * add blazor app to docker compose
    
    * add CD workflow
    
    * moved blazor app project code to /app
    prerequisites to add a test project to the solution
    
    * add test project
    
    * only publish the app project to docker image
    
    * add a test to blazor app
    
    * run test on CI
    
    * 2 tests for the same thing
    
    * commented failing test
    
    * added fmt check in blazor CI
    
    * add whitespace check in ci fmt
    
    * fmt but add useless whitespaces
    
    * fix CI
    
    * fmt
    
    * warn as error in dotnet build CI check
    
    * minor
    
    * rem counter from navigable route
    
    * enable advanced blazor features through nginx proxy

---
+++ blazor/test/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿namespace test;
+using blazor.Components.Pages;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldPass()
+    {
+        Assert.Equal(8, 8);
+    }
+    // [Fact]
+    // public void ShouldFail()
+    // {
+    //     Assert.Equal(8, 12);
+    // }
+
+    [Fact]
+    public void CounterShouldIncrementWhenClicked()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var cut = ctx.RenderComponent<Counter>();
+        var paraElm = cut.Find("p");
+
+        // Act
+        cut.Find("button").Click();
+        cut.Find("button").Click();
+        cut.Find("button").Click();
+
+        // Assert
+        var paraElmText = paraElm.TextContent;
+        paraElmText.MarkupMatches("N: 4");
+    }
+
+    [Fact]
+    public void Counter()
+    {
+        // Arrange
+        Counter c = new();
+
+        // Act
+        c.Increment();
+
+        // Assert
+        Assert.Equal(2, c.n);
+    }
+}
