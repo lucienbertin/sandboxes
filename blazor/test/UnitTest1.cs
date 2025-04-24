f49e22b41b9b5206fdfdee04c9205beacd73fd2d
Lucien Bertin <me@lucienbertin.com>
2025-04-24 17:07:01 +0200

    add test project

---
+++ blazor/test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void ShouldPass()
+    {
+        Assert.Equal(8, 8);
+    }
+    [Fact]
+    public void ShouldFail()
+    {
+        Assert.Equal(8, 8);
+    }
+}
