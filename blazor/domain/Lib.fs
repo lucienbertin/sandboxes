65d4fd9ff303ed6a010603096f52773205b0e323
Lucien Bertin <me@lucienbertin.com>
2025-04-25 10:24:24 +0200

    try add f# lib to my sln

---
+++ blazor/domain/Lib.fs
@@ -0,0 +1,10 @@
+﻿module domain = 
+    let incr (n: int) =
+        n+1
+
+[<EntryPoint>]
+let main args=
+    let n = 1
+    let incremented = domain.incr n
+    printfn "%i incremented is %i" n incremented
+    0
