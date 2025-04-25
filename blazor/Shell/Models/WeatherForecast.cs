04510e168e8d3b41e42815f21776d7972c27eed7
Lucien Bertin <me@lucienbertin.com>
2025-04-25 17:00:52 +0200

    weather model - add id and rem farenheit temp

--- blazor/Shell/Models/WeatherForecast.cs
+++ blazor/Shell/Models/WeatherForecast.cs
@@ -4,10 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Shell.Models;
 public class WeatherForecast
 {
+    public int WeatherForecastId { get; set; }
     public DateOnly Date { get; set; }
     public int TemperatureC { get; set; }
     public string? Summary { get; set; }
-
-    [Column(TypeName = "decimal(18, 2)")]
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
