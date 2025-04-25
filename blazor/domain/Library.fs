15382198ad4cc249377e2053c8e99f21d45f3b4a
Lucien Bertin <me@lucienbertin.com>
2025-04-25 10:46:01 +0200

    add basic fibonacci impl in f#

--- blazor/domain/Library.fs
+++ blazor/domain/Library.fs
@@ -1,5 +1,10 @@
 ﻿namespace domain
 
 module Incr = 
-    let incr (n: int) =
+    let incr (n: int): int =
         n+1
+
+    let rec fibonacci(n: int):int = 
+        match n with
+        | 0 | 1 -> n
+        | n -> fibonacci (n-1) + fibonacci (n - 2)
