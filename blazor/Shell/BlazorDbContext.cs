ff7447c9bd5fb9e20e7e1d0bbbc97bbf9b9c6f0f
Lucien Bertin <me@lucienbertin.com>
2025-04-25 15:55:51 +0200

    add efcore and scaffold dbcontext

---
+++ blazor/Shell/BlazorDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Shell.Models;
+
+namespace Shell;
+
+public partial class BlazorDbContext : DbContext
+{
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    public BlazorDbContext()
+    {
+
+    }
+
+    public BlazorDbContext(DbContextOptions<BlazorDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("PostgresConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
