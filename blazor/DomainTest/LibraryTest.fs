55a30550877ff41ec1d293386c4a9da2bb6833f5
Lucien Bertin <me@lucienbertin.com>
2025-04-25 12:17:38 +0200

    fix build

--- blazor/DomainTest/LibraryTest.fs
+++ blazor/DomainTest/LibraryTest.fs
@@ -1,7 +1,8 @@
 ﻿module DomainTest
 
 open NUnit.Framework
-open Domain
+open Domain.Incr
+open Domain.Fib
 
 [<SetUp>]
 let Setup () =
@@ -9,12 +10,12 @@ let Setup () =
 
 [<Test>]
 let Incr3 () =
-    Assert.That(Incr.incr 3, Is.EqualTo(4))
+    Assert.That(incr 3, Is.EqualTo(4))
 
 [<Test>]
 let Fib5 () =
-    Assert.That(Fib.fibonacci 5, Is.EqualTo(5))
+    Assert.That(fibonacci 5, Is.EqualTo(5))
 
 [<Test>]
 let Fib17 () =
-    Assert.That(Fib.fibonacci 17, Is.EqualTo(1597))
+    Assert.That(fibonacci 17, Is.EqualTo(1597))
