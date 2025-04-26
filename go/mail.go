43694f0069c144ce25f566cbc310400b77396508
Lucien Bertin <me@lucienbertin.com>
2025-04-26 23:50:30 +0200

    rewrote sendmail method so it returns an error if it cant send mail

--- go/mail.go
+++ go/mail.go
@@ -1,25 +1,21 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 
 	gomail "gopkg.in/mail.v2"
 )
 
-func sendmail(to string, subject string, body string) {
+func sendmail(to string, subject string, body string) error {
 	host := os.Getenv("SMTP_HOST")
 	port, err := strconv.ParseInt(os.Getenv("SMTP_PORT"), 10, 32)
 	if err != nil {
-		log.Fatal("port is not a valid int")
+		return errors.New("env smtp port is not a valid int")
 	}
 	from := os.Getenv("SENDER")
-
-	log.Print("env vars loaded succesfully")
-	log.Print("smtp host: " + host)
-	log.Print("port: " + strconv.Itoa(int(port)))
 
 	// Create a new message
 	message := gomail.NewMessage()
@@ -32,17 +28,12 @@ func sendmail(to string, subject string, body string) {
 	// Set email body
 	message.SetBody("text/plain", body)
 
-	log.Print("message initailised")
-
 	// Set up the SMTP dialer
 	dialer := gomail.NewDialer(host, int(port), "", "")
 
 	// Send the email
 	if err := dialer.DialAndSend(message); err != nil {
-		// log.Fatal("error sending message")
-		fmt.Println("Error:", err)
-		panic(err)
-	} else {
-		log.Print("Email sent successfully!")
+		return fmt.Errorf("mail send error: %s", err)
 	}
+	return nil
 }
