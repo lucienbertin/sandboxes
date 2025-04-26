3915702d165fba636cda6bda937c07e3ce581162
Lucien Bertin <me@lucienbertin.com>
2025-04-26 11:07:33 +0200

    basic code that sends an email through the local smtp server

--- go/main.go
+++ go/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	gomail "gopkg.in/mail.v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create a new message
+	message := gomail.NewMessage()
+
+	// Set email headers
+	message.SetHeader("From", "go@sandboxes.local")
+	message.SetHeader("To", "me@lucienbertin.com")
+	message.SetHeader("Subject", "Hello from the Mailtrap team")
+
+	// Set email body
+	message.SetBody("text/plain", "This is the Test Body")
+
+	// Set up the SMTP dialer
+	dialer := gomail.NewDialer("localhost", 25, "", "")
+
+	// Send the email
+	if err := dialer.DialAndSend(message); err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	} else {
+		fmt.Println("Email sent successfully!")
+	}
 }
