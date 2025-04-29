d9fd567f7894c25b22fa19d01074bf10975b372e
Lucien Bertin <me@lucienbertin.com>
2025-04-29 17:22:02 +0200

    naive implem for rmq publish

---
+++ nextjs/src/infrastructure/rmq/index.ts
@@ -0,0 +1,44 @@
+import client, { Channel } from "amqplib";
+const AMQP_URL = process.env.AMQP_URL as string;
+const EXCHANGE_NAME = process.env.RMQ_EXCHANGE as string;
+class RabbitMQConnection {
+    // connection!: ChannelModel;
+    channel!: Channel;
+    connected: boolean = false;
+
+    async initialize(): Promise<this> {
+        if (this.connected && this.channel) return this;
+
+        try {
+            console.log(`⌛️ Connecting to Rabbit-MQ Server`);
+            const connection = await client.connect(AMQP_URL);
+
+            console.log(`✅ Rabbit MQ Connection is ready`);
+
+            this.channel = await connection.createChannel();
+
+            console.log(`🛸 Created RabbitMQ Channel successfully`);
+
+            this.channel.assertExchange(
+                EXCHANGE_NAME,
+                "topic"
+            );
+
+            this.connected = true;
+        } catch (error) {
+            console.error(error);
+            console.error(`Not connected to MQ Server`);
+        } finally {
+            return this;
+        }
+    }
+}
+
+const mqConnection = new RabbitMQConnection();
+const isInitialized = mqConnection.initialize().then((conn) => conn.connected)
+
+export async function publish(routingKey: string, message: unknown): Promise<void> {
+    await isInitialized;
+
+    await mqConnection.channel.publish(EXCHANGE_NAME, routingKey, Buffer.from(JSON.stringify(message)));
+}
