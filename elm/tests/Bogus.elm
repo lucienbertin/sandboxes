2eaeb92cb6e703204621cb3372b4f64f7c631886
Lucien Bertin <me@lucienbertin.com>
2025-05-03 21:45:56 +0200

    add 2 bogus UT

---
+++ elm/tests/Bogus.elm
@@ -0,0 +1,19 @@
+module Bogus exposing (suite)
+
+import Expect
+import Test exposing (Test, describe, test)
+
+add1 : Int -> Int
+add1 x = 1 + x
+
+suite : Test
+suite =
+    describe "basic test"
+        [ test "1+1 = 2" <|
+            \_ -> 1 + 1
+                |> Expect.equal 2
+        , test "pipin' 1 |> add1 |> equal 2" <|
+            \_ -> 1
+                |> add1
+                |> Expect.equal 2
+    ]
