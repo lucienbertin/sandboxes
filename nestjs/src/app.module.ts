84eb394b8b569210c04d771ab523abf5a4b4cb6d
Lucien Bertin <me@lucienbertin.com>
2025-05-05 15:37:43 +0200

    split post module in 2

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { PostModule } from './post';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { GraphqlOptions } from './graphql.options';
 import { MongooseModule } from '@nestjs/mongoose';
 import { typeOrmConfig } from './typeorm.options';
+
+import { PostModule } from './post';
+import { CatModule } from './cat';
 
 @Module({
   imports: [
@@ -15,7 +17,9 @@ import { typeOrmConfig } from './typeorm.options';
     }),
     TypeOrmModule.forRoot(typeOrmConfig as TypeOrmModuleOptions),
     MongooseModule.forRoot(process.env.MONGO_URL as string),
-    PostModule,
+
+    PostModule, // vertical slice
+    CatModule, // vertical slice
   ],
 })
 export class AppModule {}
---
+++ nestjs/src/cat/cat.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CatService } from './cat.service';
+import { CatResolver } from './cat.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './cat.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
+  ],
+  providers: [CatService, CatResolver],
+})
+export class CatModule {}
