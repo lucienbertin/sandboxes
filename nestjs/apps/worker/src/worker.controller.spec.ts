40d1b9996c34ef15f5ca346cd726fc1be3c9f5fa
Lucien Bertin <me@lucienbertin.com>
2025-05-06 00:04:17 +0200

    fix test

--- nestjs/apps/worker/src/worker.controller.spec.ts
+++ nestjs/apps/worker/src/worker.controller.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkerController } from './worker.controller';
+import { WorkerService } from './worker.service';
+import { Post } from './models';
 
+class MockService {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async upsertPost(post: Post) {
+    return Promise.resolve();
+  }
+  async deletePost(post: Post) {
+    return Promise.resolve();
+  }
+
+}
 describe('WorkerController', () => {
   let controller: WorkerController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WorkerController],
+      providers: [
+        {
+          provide: WorkerService,
+          useClass: MockService,
+        },
+      ]
     }).compile();
 
     controller = module.get<WorkerController>(WorkerController);
