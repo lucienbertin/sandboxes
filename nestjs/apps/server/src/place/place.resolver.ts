f562b3d1d8860d1392d688ada010a190f0278d85
Lucien Bertin <me@lucienbertin.com>
2025-05-06 08:08:49 +0200

    add gql endpoints for place

--- nestjs/apps/server/src/place/place.resolver.ts
+++ nestjs/apps/server/src/place/place.resolver.ts
@@ -1,7 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { PlaceService } from './place.service';
+import { Place } from './place.entity';
 
 @Resolver()
 export class PlaceResolver {
-  constructor(private readonly placeService: PlaceService) {}
+  constructor(private readonly service: PlaceService) {}
+
+    @Query(() => [Place])
+    async places() {
+      return this.service.findAll();
+    }
+  
+    @Query(() => Place)
+    async place(@Args('id', { type: () => Int }) id: number) {
+      return this.service.findOneById(id);
+    }
 }
