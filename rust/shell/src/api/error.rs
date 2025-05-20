2b1a16dae1f3ddb4f6e879be21b32bba24b1eec9
Lucien Bertin <me@lucienbertin.com>
2025-05-20 23:31:47 +0200

    fmt again

--- rust/shell/src/api/error.rs
+++ rust/shell/src/api/error.rs
@@ -5,6 +5,7 @@ use rocket::response::{self, Responder, Response};
 #[rocket::async_trait]
 impl<'r> Responder<'r, 'static> for Error {
     fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+        use rocket::http::Status;
         let status = match self {
             Error::DotEnvyError(_)
             | Error::StdEnvVarError(_)
@@ -15,26 +16,26 @@ impl<'r> Responder<'r, 'static> for Error {
             | Error::LapinError(_)
             | Error::SendError(_)
             | Error::RedisError(_)
-            | Error::SerializeError(_) => rocket::http::Status::InternalServerError, // 500
+            | Error::SerializeError(_) => Status::InternalServerError, // 500
 
             #[cfg(feature = "rmqsub")]
             Error::GeoJSONSerdeError(_)
             | Error::GeoJsonError(_)
             | Error::FromUTF8Error(_)
-            | Error::Error => rocket::http::Status::InternalServerError, // 500
+            | Error::Error => Status::InternalServerError, // 500
 
-            Error::JwtError(_) | Error::AuthError(_) => rocket::http::Status::Unauthorized, // 401
+            Error::JwtError(_) | Error::AuthError(_) => Status::Unauthorized, // 401
 
-            Error::HttpError(HttpError::Conflict) => rocket::http::Status::Conflict, // 409
-            Error::HttpError(HttpError::Forbidden) => rocket::http::Status::Forbidden, // 403
-            Error::HttpError(HttpError::Gone) => rocket::http::Status::Gone,         // 410
-            Error::HttpError(HttpError::NotFound) => rocket::http::Status::NotFound, // 404
-            Error::HttpError(HttpError::NotModified) => rocket::http::Status::NotModified, // 301
-            Error::HttpError(HttpError::PreconditionFailed) => {
-                rocket::http::Status::PreconditionFailed
-            } // 412
-            Error::HttpError(HttpError::Unauthorized) => rocket::http::Status::Unauthorized, // 401
-            Error::HttpError(_) => rocket::http::Status::InternalServerError,        // 500
+            Error::HttpError(HttpError::NotModified) => Status::NotModified, // 301
+
+            Error::HttpError(HttpError::Unauthorized) => Status::Unauthorized, // 401
+            Error::HttpError(HttpError::Forbidden) => Status::Forbidden,       // 403
+            Error::HttpError(HttpError::NotFound) => Status::NotFound,         // 404
+            Error::HttpError(HttpError::Conflict) => Status::Conflict,         // 409
+            Error::HttpError(HttpError::Gone) => Status::Gone,                 // 410
+            Error::HttpError(HttpError::PreconditionFailed) => Status::PreconditionFailed, // 412
+
+            Error::HttpError(_) => Status::InternalServerError, // 500
         };
 
         Response::build().status(status).ok()
